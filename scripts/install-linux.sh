ae01321af81b224fb5f10e2f3e508ea541c11321
pablo <manishkumar97430@gmail.com>
2026-04-06 16:10:04 +0530

    Fix Tauri build flag typo

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -65,7 +65,7 @@ npm install
 
 # --- Build the App ---
 echo -e "${BLUE}Building $APP_NAME (Debian package only)...${NC}"
-npm run tauri build -- --bundle deb
+npm run tauri build -- --bundles deb
 
 # --- Locate and Install the .deb package ---
 DEB_PACKAGE=$(find src-tauri/target/release/bundle/deb -name "*.deb" | head -n 1)
